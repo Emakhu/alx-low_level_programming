faa51c43111a13e83a9c3b1e2c48868d5f1fb339
emakhu <danielemakhumhen@gmail.com>
2023-02-16 11:23:25 -0800

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 
-
 /**
  * main - UNIX is basically a simple operating system
  *
- * Return: Always 0 (success)
+ * Return: 1 (success)
  */
+
 int main(void)
 	
 {
-	printf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fprint(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
