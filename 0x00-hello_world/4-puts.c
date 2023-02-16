363cf8063119ff08b2c93c4dfa4a0aeb13480e65
emakhu <danielemakhumhen@gmail.com>
2023-02-16 07:35:42 -0800

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stolo.n>
+/**
+ * main - Programming for Hello puts
+ * Return: 0(Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
