94b2cc4f4ca837844aa898dd29a6ea40189ceb52
Daniel Emakhumhen <danielemakhumhen@gmail.com>
2023-02-22 05:20:38 +0100

    Islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,16 +2,15 @@
 
 /**
 *_islower - checks is an alphabet is lowercase or not
-*
 * @c: alphabet to check
-*
 * Return: 1 if lower, else 0
 */
 
 int _islower(int c)
+	
 {
 	if (c >= 97 && c <= 122)
-		return (1);
+	return (1);
 	else
-		return (0);
+	return (0);
 }
